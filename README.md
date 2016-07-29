04b93a4dbc487d3fce06682ef53aa8644fac9e40
Meinhardt Branig <meinhardtbranig@gmail.com>
2016-07-29 15:54:25 +0200

    added commandline arguments and updated the README.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PDF-Scan-Merger
 A little script that merges scanned pdf files together, if you scan two-sided documents with a single-sided sheet feed scanner.
+
+This is written and tested with python3.
+To run the script, pdfrw and argparse must be installed.
+You get help by using the argument `-h`.
+Because the even pages will mostly be scanned in reverse order after you scanned the odd ones and turn the whole pile around, there is the `--evenrev` argument which reverses the even pages before shuffling them together. You can do this with odd pages (`--oddrev`), too.
+
+In `examplefiles/` some example pdfs are provided to test the tool.
+
+If you would like to have some additional features, feel free to contribute or create an issue.
--- combineScans.py
+++ combineScans.py
@@ -1,24 +1,68 @@
 from pdfrw import PdfReader, PdfWriter, PageMerge
+import argparse
 
-even = PdfReader('examplefiles/even_rev.pdf')
-odd = PdfReader('examplefiles/odd.pdf')
-isEvenReversed = True;
+# Arguments
+parser = argparse.ArgumentParser(description='Process scanned pdf documents. It takes two files, one with the front and one of the back of each page and shuffles them together into one single document.')
+
+parser.add_argument('oddFile',
+                    type = str,
+                    nargs=1,
+                    help='path to the pdf with all odd pages (starting from 1)',
+                    default='examplefiles/odd.pdf')
+
+parser.add_argument('evenFile',
+                    type = str,
+                    nargs=1,
+                    help='path to the pdf with all even pages (starting from 2)',
+                    default='examplefiles/even_rev.pdf')
+
+parser.add_argument('resultFile',
+                    type = str,
+                    nargs=1,
+                    help='path and name where the shuffled file should be stored',
+                    default='examplefiles/all.pdf')
+
+parser.add_argument('--oddrev',
+                    dest='oddrev',
+                    action='store_const',
+                    const=True,
+                    default=False,
+                    help='reverses the odd pages before shuffling')
+
+parser.add_argument('--evenrev',
+                    dest='evenrev',
+                    action='store_const',
+                    const=True,
+                    default=False,
+                    help='reverses the even pages before shuffling')
+
+args = parser.parse_args()
+
+# The shuffling magic
+even = PdfReader(args.evenFile[0])
+odd = PdfReader(args.oddFile[0])
+isEvenReversed = args.evenrev;
+isOddReversed = args.oddrev;
 all = PdfWriter()
 blank = PageMerge()
 blank.mbox = [0, 0, 612, 792] # 8.5 x 11
 blank = blank.render()
 
-#all.addpage(blank)
-if isEvenReversed:
+if isEvenReversed and not isOddReversed:
     for i in range(0, len(odd.pages)):
         all.addpage(odd.pages[i])
+        all.addpage(even.pages[len(even.pages)-1-i])
+elif isOddReversed and not isEvenReversed:
+    for i in range(0, len(odd.pages)):
+        all.addpage(odd.pages[len(odd.pages)-1-i])
+        all.addpage(even.pages[i])
+elif isEvenReversed and isOddReversed:
+    for i in range(0, len(odd.pages)):
+        all.addpage(odd.pages[len(odd.pages)-1-i])
         all.addpage(even.pages[len(even.pages)-1-i])
 else:
     for x,y in zip(odd.pages, even.pages):
       all.addpage(x)
       all.addpage(y)
 
-#while len(all.pagearray) % 2:
-#  all.addpage(blank)
-
-all.write('examplefiles/all.pdf')
+all.write(args.resultFile[0])
